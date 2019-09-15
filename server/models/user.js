7916a95eeeb7be124795f4304e2bc6b8f74b8618
rushidave <rushijdave96@gmail.com>
2019-09-15 12:55:21 -0400

    Backend Book Routes

---
+++ server/models/user.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt-nodejs');
+const jwt = require('jsonwebtoken');
+const SALT_I = 10;
+
+const config = require('./../config/config').get(process.env.NODE_ENV);
+
+const userSchema = mongoose.Schema({
+    email:{
+        type: String,
+        required: true,
+        trim: true,
+        unique:1
+    },
+    password:{
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    name:{
+        type:String,
+        maxlength:100
+    },
+    lastname:{
+        type:String,
+        maxlength:100
+    },
+    role:{
+        type:Number,
+        default:0
+    },
+    token:{
+        type:String
+    }
+})
+
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = {User}
---
+++ server/server.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+const config = require('./config/config').get(process.env.NODE_ENV)
+const app = express();
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.DATABASE)
+
+const {User} = require('./models/user');
+const {Book} = require('./models/book');
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+// GET //
+app.get('/api/getbook', (req,res) =>{
+    let id = req.query.id;
+
+    Book.findById(id, (err,doc)=>{
+        if(err) return err.status(400).send(err)
+
+        res.send(doc)
+    })
+
+})
+
+app.get('/api/books', (req,res) =>{
+    //localhost:3001/api/books?skip=2&limit=5&order=asc
+    let skip = parseInt(req.query.skip);
+    let limit = parseInt(req.query.limit);
+    let order = req.query.order;
+
+    Book.find().skip(skip).sort({_id: order}).limit(limit).exec((err,doc)=>{
+        if(err) return err.status(400).send(err);
+        res.send(doc);
+    })
+
+})
+
+// POST //
+app.post('/api/book', (req,res) => {
+    const book = new Book(req.body)
+    book.save((err,doc) =>{
+        if(err) return res.status(400).send(err);
+        res.status(200).json({
+            post:true,
+            bookId: doc._id
+        })
+    })
+})
+
+// UPDATE //
+
+app.post('/api/book_update',(req,res)=>{
+    Book.findByIdAndUpdate(req.body._id, req.body, {new:true}, (err,doc)=>{
+        if(err) return res.status(400).send(err);
+        res.json({
+            success: true,
+            doc
+        })
+    })
+})
+
+// DELETE //
+
+app.delete('/api/delete_book', (req,res)=>{
+    let id = req.query.id;
+
+    Book.findByIdAndRemove(id, (err, doc)=>{
+        if(err) return res.status(400).send(err);
+        res.json(true)
+    })
+})
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, ()=>{
+    console.log(`Server Running`);
+})
