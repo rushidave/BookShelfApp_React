dab812794765c7ee49d623f3cf07eb8c5ef76308
rushidave <rushijdave96@gmail.com>
2019-09-16 17:00:03 -0400

    second commit

--- server/server.js
+++ server/server.js
@@ -51,6 +51,43 @@ app.post('/api/book', (req,res) => {
     })
 })
 
+//User Registration
+
+app.post('/api/register', (req,res)=>{
+    const user = new User(req.body);
+
+    user.save((err, doc)=>{
+        if(err) return res.json({success:false});
+        res.status(200).json({
+            success: true,
+            user: doc
+        })
+    });
+})
+
+
+app.post('/api/login',(req,res)=>{
+    User.findOne({'email': req.body.email}, (err,user)=>{
+        if(!user) return res.json({isAuth:false, message: 'Auth failed email not found'});
+
+        user.comparePassword(req.body.password, (err, isMatch)=>{
+            if(!isMatch) return res.json({
+                isAuth: false,
+                message:'Wrong Password'
+            })
+
+            user.generateToken((err, user)=>{
+                if(err) return res.status(400).send(err);
+                res.cookie('x-auth', user.token).json({
+                    isAuth: true,
+                    id: user._id,
+                    email: user.email
+                })
+            })
+        })
+    })
+})
+
 // UPDATE //
 
 app.post('/api/book_update',(req,res)=>{
